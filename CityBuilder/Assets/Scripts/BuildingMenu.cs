43d2f8050fca74b11481f0dc146f3ff906c034a8
KishoreBoopathi <60489284+KishoreBoopathi@users.noreply.github.com>
2020-09-14 15:29:45 -0400

    HUD Implementation

---
+++ CityBuilder/Assets/Scripts/BuildingMenu.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public static class ButtonExtension
+{
+    public static void AddEventListener<T>(this Button button, T param, Action<T> OnClick)
+    {
+        button.onClick.AddListener(delegate ()
+        {
+            OnClick(param);
+        });
+    }
+}
+public class BuildingMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public List<BuildingsData> buildingsList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject actionButton = transform.GetChild(0).gameObject;
+        GameObject backButton = transform.GetChild(1).gameObject;
+        GameObject actionSlot;
+        for (int i = 0; i < buildingsList.Count; i++)
+        {
+            actionSlot = Instantiate(actionButton, transform);
+            actionSlot.transform.GetChild(0).GetComponent<Image>().sprite = buildingsList[i].Model.GetComponent<Image>().sprite;
+            actionSlot.transform.GetChild(1).GetComponent<Text>().text = buildingsList[i].Name;
+
+            /*actionSlot.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                SlotPressed(buildingsList[i]);
+            });*/
+
+            actionSlot.GetComponent<Button>().AddEventListener(buildingsList[i], SlotPressed);
+        }
+        actionSlot = Instantiate(backButton, transform);
+        Destroy(actionButton);
+        Destroy(backButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SlotPressed(BuildingsData building)
+    {
+        Debug.Log("Building Name: " + building.Name);
+        FindObjectOfType<CubePlacer>().Prefab = building.Model;
+        FindObjectOfType<GameManager>().TakeAway(building.StonePrice, building.WoodPrice);
+    }
+
+    public void OnBackButtonPressed()
+    {
+        this.gameObject.SetActive(false);
+        mainMenu.SetActive(true);
+
+    }
+}
