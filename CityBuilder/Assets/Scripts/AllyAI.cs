a42126b8752b081662fc1e6ed32589f3b2a2ee70
Raj Shah <rvshah269@gmail.com>
2020-09-15 09:16:18 -0400

    AI

---
+++ CityBuilder/Assets/Scripts/AllyAI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AllyAI : MonoBehaviour
+{
+    public Transform Follow;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Vector2 movement;
+    public Collider2D col;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            Follow = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        else
+        {
+            Follow = GameObject.FindGameObjectWithTag("Enemy").transform;
+        }
+        Vector2 dir = (Vector2)Follow.position - rb.position;
+        
+        movement = dir.normalized;
+    }
+    void FixedUpdate()
+    {
+        Movement(movement); 
+    }
+    public void Movement(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Damage To Enemy Here");
+        }
+        if(collision.gameObject.tag == "Player")
+        {
+            movement = Vector2.zero;
+        }
+    }
+    
+}
--- CityBuilder/Assets/Scripts/EnemyAI.cs
+++ CityBuilder/Assets/Scripts/EnemyAI.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class AI : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     public Transform player;
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Vector2 movement;
     public Collider2D col;
+    public Animator animator;
     // Start is called before the first frame update
 
 
@@ -17,23 +18,31 @@ public class AI : MonoBehaviour
     void Update()
     {
         Vector2 dir = (Vector2)player.position - rb.position;
+        if(animator != null)
+        {
+            if (dir.x <= 0)
+            {
+                animator.SetFloat("Horizontal", -1f);
+            }
+            else if (dir.x > 0)
+            {
+                animator.SetFloat("Horizontal", 1f);
+            }
+        }
+        
+        
         movement = dir.normalized;
     }
     void FixedUpdate()
     {
-        Movement(movement); 
+        Movement(movement);
+        
+
     }
     public void Movement(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
-    /*private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            Debug.Log("Damage Here");
-        }
-    }*/
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
