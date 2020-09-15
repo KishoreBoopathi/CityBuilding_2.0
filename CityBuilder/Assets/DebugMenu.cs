9b2e09a5daf7668bb2a6473696057cffb6c79ac3
YoungSensei <59657629+YoungSensei@users.noreply.github.com>
2020-09-15 12:31:47 -0400

    debug menu

---
+++ CityBuilder/Assets/DebugMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugMenu : MonoBehaviour
+{
+   public void Next()
+    {
+        if (SceneManager.GetActiveScene().buildIndex+1< SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            Debug.Log("No more Scenes.");
+    }
+
+    public void Previous()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
