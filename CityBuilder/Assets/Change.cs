c31a88d7a9fbf723ad9c6b134272763c4d73a84c
YoungSensei <59657629+YoungSensei@users.noreply.github.com>
2020-09-25 14:37:40 -0400

    sure

---
+++ CityBuilder/Assets/Change.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : MonoBehaviour
+{
+    public GameObject cm,cm2 ;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            cm.SetActive(false);
+            cm2.SetActive(true);
+        }
+    }
+}
---
+++ CityBuilder/Assets/chest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class chest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
